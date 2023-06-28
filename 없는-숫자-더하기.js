3dfd047f78e3540e19a246655ad5c97f450ff87d
hyunwoo <pho9902@naver.com>
2023-06-28 23:47:46 +0900

    test

--- 없는-숫자-더하기.js
+++
@@ -1,9 +0,0 @@
-function solution(numbers) {
-    let answer = 0;
-    for (i = 0; i <= 9; i++) {
-        if( -1 === numbers.indexOf(i)) {
-            answer += i;
-        }
-    }
-    return answer;
-}
